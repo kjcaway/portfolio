6fbbb9e2c472ca56315029d19eb7ee7377ebce82
kjcaway <whdckstngh@naver.com>
2019-05-01 15:48:19 +0900

    add logging module, db pool event logic

--- server/app.js
+++ server/app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const api = require('./routes/index');
+const logger = require('./logger')
 
 app.use(bodyParser.json());
 app.use('/api', api);
 
+// error handler
+app.use(function (err, req, res, next) {
+  logger.error('Error handler : ', err);
+  res.status(500).send('Something broke!');
+});
+
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => logger.info(`Listening on port ${port}...`));
--- server/db.js
+++ server/db.js
@@ -1,8 +1,21 @@
 const mysql = require("mysql");
 const config = require("./config/config");
+const logger = require('./logger');
 
 const pool = mysql.createPool(config);
-console.log('Connection pool created.');
+logger.info('Connection pool created.');
+
+pool.on('acquire', function (connection) {
+  logger.info('Connection %d acquired', connection.threadId);
+});
+
+pool.on('enqueue', function () {
+  logger.info('Waiting for available connection slot');
+});
+
+pool.on('release', function (connection) {
+  logger.info('Connection %d released', connection.threadId);
+});
 
 const getConn = function(callback) {
   pool.getConnection(function(err, connection) {
